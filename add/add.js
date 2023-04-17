8ac5cf6cdabc2b9902f3eb94ae2c83204ffc042b
Nvdcloud <84348016+Nvdcloud@users.noreply.github.com>
2023-04-17 17:31:18 +0100

    Update add.js
    update to addition function to accept arrays. added for loop.

--- add/add.js
+++ add/add.js
@@ -1,11 +1,19 @@
-/*1. Write a function that returns the sum of two numbers.
-2. Write a function that returns the sum of all numbers regardless of # of params.
-*/
+    /*1. Write a function that returns the sum of two numbers.
+    2. Write a function that returns the sum of all numbers regardless of # of params.
+    */
 
-function additionAll(num, num2){
-    let add = num + num2
-    console.log(add)
-}
+    */
 
-
-additionAll(1, 1)
+    function additionAll(num, num2, ...arr){
+        let add = num + num2
+        for(let i = 0; i < arr.length; i++){
+            add += arr[i]
+        }
+        
+        console.log(add)
+    }
+    
+    let arr = [10,10,10,10,10]
+    
+    additionAll(1, 2,...arr)
+    
