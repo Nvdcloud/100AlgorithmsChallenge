13b730cf9fbe22a775532f911d6361cec0eb23d4
Nvdcloud <84348016+Nvdcloud@users.noreply.github.com>
2023-04-17 07:42:25 +0100

    Create add.js
    Writing function that returns sum regardless of params.

---
+++ add/add.js
@@ -0,0 +1,11 @@
+/*1. Write a function that returns the sum of two numbers.
+2. Write a function that returns the sum of all numbers regardless of # of params.
+*/
+
+function additionAll(num, num2){
+    let add = num + num2
+    console.log(add)
+}
+
+
+additionAll(1, 1)
